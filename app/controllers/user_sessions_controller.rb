8458558eaca7fadf37f601ef3f2fa3e7b8d8a35a
Jason LaPier <jason.lapier@gmail.com>
2009-09-24 11:24:26 -0700

    layout tweaks with sig palette

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default account_url
+      redirect_back_or_default '/'
     else
       flash[:error] = "Unable to login. Bad username or password?"
       redirect_to login_url
