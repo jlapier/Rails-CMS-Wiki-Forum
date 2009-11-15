f38b777ec5f9c4d381c5caa37e6d550dc95c53e5
Jason LaPier <jason.lapier@gmail.com>
2009-11-15 09:29:33 -0800

    adding a default css that can be overridden

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -15,7 +15,15 @@ class ThemesController < ApplicationController
   end
 
   def css_editor
-    @css = "body { background: #EFA }"
+    @css = SiteSetting.read_setting('css override') ||
+      File.read( File.join(RAILS_ROOT, 'public', 'stylesheets', 'main_elements.css') )
+  end
+
+  def update_css
+    SiteSetting.write_setting('css override', params[:css])
+    SiteSetting.write_setting('css override timestamp', Time.now.to_i)
+    flash[:notice] = "CSS override updated."
+    redirect_to :action => :css_editor
   end
 
   def css
