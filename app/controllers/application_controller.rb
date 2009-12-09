37662f267e116c1dfa9d518a50fd3d1441f5deae
Jason LaPier <jason.lapier@gmail.com>
2009-12-09 08:10:55 -0800

    add the exception notifier plugin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include ExceptionNotifiable
+
   helper :all
   helper_method :current_user_session, :current_user
   filter_parameter_logging :password, :password_confirmation
--- config/initializers/core_ext.rb
+++ config/initializers/core_ext.rb
@@ -1,3 +1,8 @@
 # include stuff here
 include YourConstants
 include ColorConstants
+
+
+ExceptionNotifier.exception_recipients = %w(jason.lapier@tacs.uoregon.edu)
+ExceptionNotifier.email_prefix = "[SIGSITE ERROR] "
+
