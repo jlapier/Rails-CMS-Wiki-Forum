8b5485efbdecab76a519855a05fa438eedde7e45
Jeremiah Heller <jeremiah@inertialbit.net>
2010-11-24 12:12:43 -0800

    define Wiki(Page) abilities & auth/z link wrappers for Wikis

--- app/helpers/wiki_pages_helper.rb
+++ app/helpers/wiki_pages_helper.rb
@@ -1,2 +1,31 @@
 module WikiPagesHelper
+  def link_to_new_wiki_page(wiki, options={})
+    if can? :create, WikiPage.new, wiki
+      text = options.delete(:text) || "create new page"
+      link = link_to(text, new_wiki_wiki_page_path(wiki))
+      link = "<p>[#{link}]</p>".html_safe if options.delete :pb
+      link
+    end
+  end
+  
+  def link_to_edit_wiki_page(wiki_page, options={})
+    if can? :update, wiki_page
+      link_to 'edit', edit_wiki_wiki_page_path(wiki_page.wiki, wiki_page)
+    end
+  end
+  
+  def link_to_delete_wiki_page(wiki_page, options={})
+    if can? :delete, wiki_page
+      link_to 'delete', wiki_wiki_page_path(wiki_page.wiki, wiki_page), {
+        :confirm => "Are you really sure you want to delete this wiki page?",
+        :method => :delete
+      }
+    end
+  end
+  
+  def link_to_wiki_page_history(wiki_page, options={})
+    if can? :read, wiki_page
+      link_to 'history', history_wiki_wiki_page_path(wiki_page.wiki, wiki_page)
+    end
+  end
 end
--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,57 @@
 module WikisHelper
+  def link_to_wiki(wiki, options={})
+    return '' unless current_user
+    link_body = options.delete(:text) || wiki.name
+    link_to_if(can?(:read, wiki), link_body, wiki)
+  end
+  def link_to_new_wiki
+    return '' unless current_user
+    if can?(:create, Wiki.new)
+      link_to 'New wiki', new_wiki_path
+    end
+  end
+  def link_to_edit_wiki(wiki, options={})
+    return '' unless current_user
+    
+    # todo review cancan implementation
+    if can? :update, wiki
+      html_body = '<span class="ui-icon ui-icon-document"/>'.html_safe
+      text_body = 'Edit Wiki Details'
+      link_body = options.delete(:html) ? html_body : text_body
+      link_to link_body, edit_wiki_path(wiki), :title => 'click to edit'
+    end
+  end
+  
+  def link_to_destroy_wiki(wiki, options={})
+    return '' unless current_user
+    
+    if can? :destroy, wiki
+      html_body = '<span class="ui-icon ui-icon-closethick"/>'.html_safe
+      text_body = 'delete'
+      link_body = options.delete(:html) ? html_body : text_body
+      link_to link_body, wiki_path(wiki), {
+        :confirm => "Are you sure you want to permanently delete this wiki (#{wiki.name}) " +
+          "and it's #{pluralize wiki.wiki_pages.count, 'page'}?",
+        :method => :delete,
+        :title => "click to delete"
+      }
+    end
+  end
+
+  def link_to_wiki_comments(wiki, options={})
+    if can? :read, wiki
+      link_to 'chatter', wiki_wiki_comments_path(wiki)
+    end
+  end
+  
+  def link_to_wiki_feeds
+    content_tag :span, :class => 'feed_links' do
+      link_to image_tag('feed-icon.gif') + " all", wiki_wiki_comments_url(@wiki, :format => :atom),
+            :title => 'Subscribe to feed for all activity'
+      link_to image_tag('feed-icon.gif') + " daily", daily_wiki_wiki_comments_url(@wiki, :format => :atom),
+            :title => 'Subscribe to feed for daily digest'
+      link_to image_tag('feed-icon.gif') + " weekly", weekly_wiki_wiki_comments_url(@wiki, :format => :atom),
+            :title => 'Subscribe to feed for weekly digest'
+    end
+  end
 end
