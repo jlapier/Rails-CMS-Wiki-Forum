54684cd01550c0eca4bf3e58a1a098efd4a1182d
Jeremiah Heller <jeremiah@inertialbit.net>
2010-11-29 09:22:59 -0800

    split events into future/past columns

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   before_filter :require_admin_user
   
   def event_calendar
-    @events = Event.order("start_on DESC")
+    @past_events = Event.past.order("start_on ASC")
+    @future_events = Event.future.order("start_on ASC")
   end
 end
