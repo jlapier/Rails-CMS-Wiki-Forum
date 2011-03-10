63401c47cb5526e70c5792d7afc745dfcbb485b0
Jeremiah Heller <jeremiah@inertialbit.net>
2011-03-10 14:32:35 -0800

    silence deprecation warnings for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,3 +33,4 @@ RailsCMSWikiForum::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 end
+ActiveSupport::Deprecation.silenced = true
