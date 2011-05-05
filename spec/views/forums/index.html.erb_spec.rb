aff0146c762d265a255cb1656351c9231f95a325
Jeremiah Heller <jeremiah@inertialbit.net>
2011-05-04 17:57:27 -0700

    display most recent message subject per forum on forums#index

--- spec/views/forums/index.html.erb_spec.rb
+++ spec/views/forums/index.html.erb_spec.rb
@@ -4,24 +4,36 @@ describe "/forums/index.html.erb" do
   include ForumsHelper
 
   def mock_user(stubs={})
-    @mock_user ||= mock_model(User, stubs.merge({:is_admin? => true}))
+    @mock_user ||= mock_model(User, stubs.merge({:name => 'User',:is_admin? => true}))
   end
 
   before(:each) do
+    @message1 = stub_model(MessagePost, {
+      :subject => 'Subject 1',
+      :body => 'I am Message 1',
+      :user => mock_user,
+      :created_at => Date.yesterday
+    })
+    @message2 = stub_model(MessagePost, {
+      :subject => '2 Subject',
+      :body => '2 Message am I',
+      :user => mock_user,
+      :created_at => 2.days.ago
+    })
     @forums = [
       stub_model(Forum,
         :title => "value for title 1",
         :description => "value for description 1",
         :position => 1,
         :moderator_only => false,
-        :newest_message_post_id => 1
+        :most_recent_post => @message1
       ),
       stub_model(Forum,
         :title => "value for title 2",
         :description => "value for description 2",
         :position => 1,
         :moderator_only => false,
-        :newest_message_post_id => 1
+        :most_recent_post => @message2
       )
     ]
     assign(:forums, @forums)
@@ -34,5 +46,10 @@ describe "/forums/index.html.erb" do
     rendered.should =~ (/value for description 1/)
     rendered.should =~ (/value for title 2/)
     rendered.should =~ (/value for description 2/)
+  end
+  it "renders the most recent message subject from each forum" do
+    render
+    rendered.should =~ (/Subject 1/)
+    rendered.should =~ (/2 Subject/)
   end
 end
