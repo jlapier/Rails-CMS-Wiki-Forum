2a5766d2972c44ecb2dcaf6d19e0e6aa0aca9c49
Jeremiah Heller <jeremiah@inertialbit.net>
2011-05-05 01:04:20 -0700

    show all blog post links to any logged in user

--- app/helpers/blog/posts_helper.rb
+++ app/helpers/blog/posts_helper.rb
@@ -1,24 +1,24 @@
 module Blog::PostsHelper
   def link_to_new_post
-    if can? :create, Blog::Post.new
+    if current_user and current_user.logged_in? # can? :create, Blog::Post.new
       link_to 'New Post', new_blog_post_path
     end
   end
   
   def link_to_delete_post(post)
-    if can? :delete, post
+    if current_user and current_user.logged_in? # can? :delete, post
       link_to 'Delete', blog_post_path(post), :method => :delete, :confirm => "Delete post: #{post.title}?"
     end
   end
   
   def link_to_edit_post(post)
-    if can? :edit, post
+    if current_user and current_user.logged_in? # can? :edit, post
       link_to 'Edit', edit_blog_post_path(post)
     end
   end
   
   def link_to_read_more(post)
-    if can? :read, post
+    if post.published or current_user.logged_in?
       link_to "Read more...", blog_post_path(post)
     end
   end
