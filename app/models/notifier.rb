1592efa571d08346767df89f87564c9058333551
Jason LaPier <jason.lapier@gmail.com>
2011-08-16 11:58:00 -0700

    quick fix for sending emails

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -11,7 +11,7 @@ public
   def password_reset_instructions(user, sent_at = Time.now)
     subject    'Password Reset Instructions'
     recipients user.email
-    from       SiteSetting.read_setting('site title')
+    from       SiteSetting.read_setting('site email') || 'root'
     sent_on    sent_at
     
     @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
@@ -20,7 +20,7 @@ public
   def user_created(user, sent_at = Time.now)
     subject    "New user registered: #{user.fullname}"
     recipients admins
-    from       SiteSetting.read_setting('site title')
+    from       SiteSetting.read_setting('site email') || 'root'
     sent_on    sent_at
 
     @edit_user_url = edit_user_url(user)
@@ -31,7 +31,7 @@ public
   def published_blog_post_updated(post, updating_user)
     subject "Published Blog Post Updated [#{post.title.truncate(23)}]"
     recipients admins
-    from SiteSetting.read_setting('site title')
+    from     SiteSetting.read_setting('site email') || 'root'
     
     @publisher_name = "#{updating_user.first_name} #{updating_user.last_name}"
     @post_title = post.title
